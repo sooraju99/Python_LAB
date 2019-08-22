b018e55e877042ef695074bdf27d4ea0edd34eb2
sooraju99 <47816525+sooraju99@users.noreply.github.com>
2019-08-22 12:30:59 +0530

    Add files via upload

---
+++ 1B (Fibonacci).py
@@ -0,0 +1,22 @@
+#Program to display N fibonacci numbers
+def Fibonacci(n): 
+    if n<0: 
+        print("Wrong input") 
+
+    elif n==1: 
+        return 0
+
+    elif n==2: 
+        return 1
+    else: 
+        return Fibonacci(n-1)+Fibonacci(n-2) 
+
+num =int(input("Enter the number : "))
+print "Fibonacci Series is : "
+i = 1
+while i <= num:
+		print(Fibonacci(i))
+		i = i + 1
+
+  
+
